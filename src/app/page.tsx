e476ded3403423585485606a189b29daeecc3123
Yevhenii Pozniak <jekamanu@gmail.com>
2025-08-05 14:37:25 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./styles/home.module.css";
 
 export default function Home() {
@@ -9,15 +10,29 @@ export default function Home() {
           <h2 className={styles.text}>
             You can find everything you want in our catalog
           </h2>
+          <Link
+            href="/catalog"
+            className={styles.styled_link}
+            style={{ width: 173 }}
+          >
+            View Now
+          </Link>
         </div>
       </section>
 
       <section className={styles.section}>
         <div className={styles.second_wrapper}>
-          <h1 className={styles.title}>Campers of your dreams</h1>{" "}
+          <h1 className={styles.title}>Campers of your dreams</h1>
           <h2 className={styles.text}>
             You can find everything you want in our catalog
           </h2>
+          <Link
+            href="/catalog"
+            className={styles.styled_link}
+            style={{ width: 166 }}
+          >
+            View Now
+          </Link>
         </div>
       </section>
     </>
