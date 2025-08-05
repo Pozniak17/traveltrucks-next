c158669383e02be953f61c452f765a6c0fcfac8b
Yevhenii Pozniak <jekamanu@gmail.com>
2025-08-05 14:19:17 +0300

    add Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "modern-normalize";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // обери, які потрібні
+  variable: "--font-inter", // опціонально — як CSS-змінна
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import styles from "./styles/home.module.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <section className={styles.hero}>
+        <div className={styles.wrapper}>
+          <h1 className={styles.title}>Campers of your dreams</h1>
+          <h2 className={styles.text}>
+            You can find everything you want in our catalog
+          </h2>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.second_wrapper}>
+          <h1 className={styles.title}>Campers of your dreams</h1>{" "}
+          <h2 className={styles.text}>
+            You can find everything you want in our catalog
+          </h2>
+        </div>
+      </section>
+    </>
   );
 }
